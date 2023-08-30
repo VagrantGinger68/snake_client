99121af9af9ba9c34e6ead3dc38dbca0e04708f2
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 15:08:43 -0400

    imported IP and PORT from constants

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants")
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -13,10 +14,6 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to server!");
     conn.write("Name: RJS");
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 50);
-
   });
 
   conn.on("data", (data) => {
