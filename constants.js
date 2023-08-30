6b8b40ca735cf67d319f803907c4991933c45568
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 15:08:14 -0400

    Put constant variables in seperate file

---
+++ constants.js
@@ -0,0 +1,19 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const UP = "Move: up";
+const DOWN = "Move: down";
+const LEFT = "Move: left";
+const RIGHT = "Move: right";
+const HELLO = "Say: Hello";
+const GG = "Say: Good Game!";
+
+module.exports = {
+  IP,
+  PORT,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  HELLO,
+  GG
+};
