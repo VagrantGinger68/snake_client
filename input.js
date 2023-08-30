64f3a340ae15f569f61a8b29cb8085d7a7bb1b94
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 14:40:48 -0400

    added a connection to the server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,18 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w' || key === "\u001b[A") {
+    connection.write("Move: up");
+  }
+  if (key === 'a' || key === "\u001b[D") {
+    connection.write("Move: left");
+  }
+  if (key === 's' || key === "\u001b[B") {
+    connection.write("Move: down");
+  }
+  if (key === 'd' || key === "\u001b[C") {
+    connection.write("Move: right");
   }
 };
 
