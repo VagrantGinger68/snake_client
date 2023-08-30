1d52e07d68095a5f6624daf17a1f8373326e9187
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 15:34:19 -0400

    added an interval

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 const { UP, DOWN, LEFT, RIGHT, HELLO, GG } = require("./constants");
 
 let connection;
+let clear;
 
 const setupInput = function(conn) {
   connection = conn;
@@ -12,22 +13,32 @@ const setupInput = function(conn) {
   return stdin;
 };
 
+
 const handleUserInput = function(key) {
+  const repeat = function(key) {
+    clear = setInterval(() => {
+      connection.write(key);
+    }, 100);
+  };
 
   if (key === '\u0003') {
     process.exit();
   }
   if (key === 'w' || key === "\u001b[A") {
-    connection.write(UP);
+    clearInterval(clear);
+    repeat(UP);
   }
   if (key === 'a' || key === "\u001b[D") {
-    connection.write(LEFT);
+    clearInterval(clear);
+    repeat(LEFT);
   }
   if (key === 's' || key === "\u001b[B") {
-    connection.write(DOWN);
+    clearInterval(clear);
+    repeat(DOWN);
   }
   if (key === 'd' || key === "\u001b[C") {
-    connection.write(RIGHT);
+    clearInterval(clear);
+    repeat(RIGHT);
   }
   if (key === 'g') {
     connection.write(GG);
