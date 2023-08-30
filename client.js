06607484fdc22c76eba67ff093545ce90ba99954
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-30 13:32:02 -0400

    added a name variable upon connection

--- client.js
+++ client.js
@@ -10,6 +10,12 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to server!");
+    conn.write("Name: RJS");
+
+  });
+
   conn.on("data", (data) => {
     console.log(data);
   });
